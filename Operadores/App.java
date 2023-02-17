2e99cae7e8f11aa089744e26ebf170f1a3cab57b
DanielLRomano <romanodaniel1012@gmail.com>
2023-02-17 20:15:27 -0300

    a

---
+++ Operadores/App.java
@@ -0,0 +1,11 @@
+package Java.Operadores;
+
+/**
+ * App
+ */
+public class App {
+    public static void main(String[] args) {
+        Operacoes operador = new Operacoes();
+        operador.aritimetica();
+    }
+}
