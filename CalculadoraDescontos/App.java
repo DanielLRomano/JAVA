4196ee7b8f80939f83c33fd40b5eea8642372c8d
DanielLRomano <romanodaniel1012@gmail.com>
2023-02-17 19:36:22 -0300

    Added

--- CalculadoraDescontos/App.java
+++ CalculadoraDescontos/App.java
@@ -1,5 +1,11 @@
 package Java.CalculadoraDescontos;
 
 public class App {
-    
+    public static void main(String[] args) {
+
+        CalculadoraDescontos calcular = new CalculadoraDescontos();
+        calcular.Calcular20();
+        calcular.Calcular30();
+    }
+
 }
--- CalculadoraDescontos/CalculadoraDescontos.java
+++ CalculadoraDescontos/CalculadoraDescontos.java
@@ -1,5 +1,26 @@
 package Java.CalculadoraDescontos;
 
+import java.util.Scanner;
+
 public class CalculadoraDescontos {
-    
+
+    Scanner sc = new Scanner(System.in);
+
+    public void Calcular20() {
+        System.out.println("Qual o valor do produto?");
+        double precoProduto = sc.nextDouble();
+        System.out.println("Será aplicado 20% de desconto");
+        final int desconto20 = 20;
+        double precoComDesconto = precoProduto - (precoProduto * desconto20 / 100);
+        System.out.println("O Preço com desconto é: " + precoComDesconto + " Reais");
+    }
+
+    public void Calcular30() {
+        System.out.println("Qual o valor do produto?");
+        double precoProduto = sc.nextDouble();
+        System.out.println("Será aplicado 30% de desconto");
+        final int desconto30 = 30;
+        double precoComDesconto = precoProduto - (precoProduto * desconto30 / 100);
+        System.out.println("O Preço com desconto é: " + precoComDesconto + " Reais");
+    }
 }
