c9ca2bb3f5e75f04f295298ed62203590d574105
DanielLRomano <romanodaniel1012@gmail.com>
2023-02-24 19:44:38 -0300

    add

--- Desafio1/App.java
+++ Desafio1/App.java
@@ -4,5 +4,5 @@ public class App {
     public static void main(String[] args) {
         desafio1 objeto1 = new desafio1();
         objeto1.aprovacao();
-    } 
+    }
 }
--- Desafio1/desafio1.java
+++ Desafio1/desafio1.java
@@ -8,19 +8,29 @@ public class desafio1 {
 
     public void aprovacao() {
 
-        System.out.println("Informe sua primeira nota: ");
+        System.out.println("Informe a primeira nota do Aluno: ");
         double nota1 = sc.nextDouble();
 
-        System.out.println("Informe sua segunda nota: ");
+        System.out.println("Informe a segunda nota do Aluno: ");
         double nota2 = sc.nextDouble();
 
-        System.out.println("Informe sua frequencia: ");
+        System.out.println("Informe a frequencia do Aluno: ");
         int frequencia = sc.nextInt();
 
         double media = (nota1 + nota2) / 2;
 
+        System.out.println("A média do aluno foi: " + media);
+
+        System.out.println("A frequêcia do aluno foi: " + frequencia + "%");
+
         boolean aprovado = media >= 50 && frequencia >= 75;
 
-        System.out.println("O aluno foi aprovado: " + aprovado);      
-    } 
+        if (aprovado == true) {
+            System.out.println("O aluno foi aprovado!");
+        } else {
+            System.out.println("O aluno foi reprovado!");
+        }
+
+        sc.close();
+    }
 }
