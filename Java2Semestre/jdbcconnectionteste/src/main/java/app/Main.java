61ed1f7c8dc73421e4ed4f7044d2da6042c679e7
DanielLRomano <romanodaniel1012@gmail.com>
2023-11-08 21:59:12 -0300

    08/11

--- Java2Semestre/jdbcconnectionteste/src/main/java/app/Main.java
+++ Java2Semestre/jdbcconnectionteste/src/main/java/app/Main.java
@@ -4,7 +4,11 @@ import Connection.ConnectionDAO;
 
 public class Main {
     public static void main(String[] args) {
-        new ConnectionDAO().criarTabela();
-        new ConnectionDAO().inserir("Daniel", "romanodaniel1012@gmail.com");
+        // new ConnectionDAO().criarTabela();
+        // new ConnectionDAO().inserir("Daniel", "romanodaniel1012@gmail.com");
+        // new ConnectionDAO().buscarPorId(1);
+        // new ConnectionDAO().apagarID(1);
+        // new ConnectionDAO().atualizarID(2, "joao", "joao@joao");
+        new ConnectionDAO().listarTodos();
     }
 }
